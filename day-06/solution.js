66d82de8d7417078367400057cf266d119f3bf0f
tmanso9 <tmanso92@gmail.com>
2023-12-06 12:51:35 +0000

    level 2 working

--- day-06/solution.js
+++ day-06/solution.js
@@ -2,27 +2,26 @@ const { readData } = require('../utils/helper')
 
 const data = readData('input.txt')
 
-console.log(data)
+const firstLevel = () => {
+	
+}
 
-const time = data.shift().split(':')[1].trim().match(/\d+/g).map(Number)
-const distance = data.shift().split(':')[1].trim().match(/\d+/g).map(Number)
+const secondLevel = () => {
+	const raceTime = parseInt(
+		data.shift().split(':')[1].trim().replaceAll(' ', '')
+	)
+	const recordDistance = parseInt(
+		data.shift().split(':')[1].trim().replaceAll(' ', '')
+	)
 
-const waysToWin = []
-
-for (let i = 0; i < time.length; i++) {
-	const raceTime = time[i]
-	const recordDistance = distance[i]
-	// console.log(raceTime, recordDistance)
-	let winningOptions = 0
+	let waysToWin = 0
 	for (let j = 0; j < raceTime; j++) {
 		const remainingTime = raceTime - j
 		const points = remainingTime * j
-		// console.log(points)
-		if (points > recordDistance) winningOptions++
+		if (points > recordDistance) waysToWin++
 	}
-	waysToWin.push(winningOptions)
+
+	console.log(`Second level solution:\t${waysToWin}`)
 }
 
-// console.log(waysToWin)
-const totalWays = waysToWin.reduce((acc, val) => acc * val, 1)
-console.log(`There are ${totalWays} ways to win`)
+secondLevel()
