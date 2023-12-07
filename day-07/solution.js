fa889f0d7bd675a4636f2bebe6d6ce515bcbf2c1
tmanso9 <tmanso92@gmail.com>
2023-12-07 19:24:38 +0000

    Level 2 done, very ugly code

--- day-07/solution.js
+++ day-07/solution.js
@@ -16,7 +16,7 @@ class Hand {
 	constructor(cards, bid) {
 		this.cards = cards
 		this.bid = bid
-		this.type = []
+		this.type = TYPES.HIGH
 		this.cardsAmount = []
 	}
 }
@@ -28,14 +28,17 @@ for (const line of data) {
 	hands.push(new Hand(cards, parseInt(bid)))
 }
 
-const checkType = (hand) => {
-	const cards = new Map()
-
+const mapHands = (hand) => {
 	for (const letter of hand.cards) {
 		cards.get(letter) === undefined
 			? cards.set(letter, 1)
 			: cards.set(letter, cards.get(letter) + 1)
 	}
+}
+
+const checkType = (hand) => {
+	cards.clear()
+	mapHands(hand)
 
 	hand.cardsAmount = Array.from(cards).sort((a, b) => b[1] - a[1])
 	// console.log(hand.cardsAmount)
@@ -66,9 +69,97 @@ const checkType = (hand) => {
 	}
 }
 
+const checkTypeWithJoker = (hand) => {
+	cards.clear()
+	mapHands(hand)
+
+	hand.cardsAmount = Array.from(cards).sort((a, b) => b[1] - a[1])
+	// console.log(hand.cardsAmount)
+
+	const jokers = cards.get('J') || 0
+
+	const cardsWithoutJoker = hand.cardsAmount.filter((card) => card[0] !== 'J')
+	// console.log(`Without joker:`)
+	// console.log(cardsWithoutJoker)
+	// console.log(`jokers: ${jokers}`)
+
+	if (cardsWithoutJoker && cardsWithoutJoker.length) {
+		switch (cardsWithoutJoker[0][1]) {
+			case 5:
+				hand.type = TYPES.FIVE
+				break
+			case 4:
+				if (jokers === 1) hand.type = TYPES.FIVE
+				else hand.type = TYPES.FOUR
+				break
+			case 3:
+				switch (jokers) {
+					case 2:
+						hand.type = TYPES.FIVE
+						break
+					case 1:
+						hand.type = TYPES.FOUR
+						break
+					default:
+						if (cardsWithoutJoker[1][1] === 2) {
+							hand.type = TYPES.FULL
+						} else {
+							hand.type = TYPES.THREE
+						}
+				}
+				break
+			case 2:
+				switch (jokers) {
+					case 3:
+						hand.type = TYPES.FIVE
+						break
+					case 2:
+						hand.type = TYPES.FOUR
+						break
+					case 1:
+						if (cardsWithoutJoker[1][1] === 2) {
+							hand.type = TYPES.FULL
+						} else {
+							hand.type = TYPES.THREE
+						}
+						break
+					default:
+						if (cardsWithoutJoker[1][1] === 2) {
+							hand.type = TYPES.TWO
+						} else {
+							hand.type = TYPES.ONE
+						}
+					}
+				break
+			default:
+				switch (jokers) {
+					case 4:
+						hand.type = TYPES.FIVE
+						break
+					case 3:
+						hand.type = TYPES.FOUR
+						break
+					case 2:
+						hand.type = TYPES.THREE
+						break
+					case 1:
+						hand.type = TYPES.ONE
+						break
+					default:
+						hand.type = TYPES.HIGH
+				}
+		}
+	} else {
+		// console.log('aqui')
+		hand.type = TYPES.FIVE
+	}
+
+	// console.log(hand.type)
+}
+
 let totalWinnings = 0
 
-const orderedByStrength = ((strength) => {
+const orderedByStrength = (strength) => {
 	const orderedHands = hands.sort((a, b) => {
 		if (a.type != b.type) return a.type - b.type
 		for (let i = 0; i < a.cards.length; i++) {
@@ -81,6 +172,16 @@ const orderedByStrength = ((strength) => {
 	})
 
 	return orderedHands
+}
+
+const calcStrengthAndPrint = ((strength, level) => {
+	const orderedHands = orderedByStrength(strength)
+
+	orderedHands.forEach((hand, index) => {
+		totalWinnings += hand.bid * (index + 1)
+	})
+
+	console.log(`${level} level solution:\t`, totalWinnings)
 })
 
 const firstLevel = () => {
@@ -89,13 +190,20 @@ const firstLevel = () => {
 	}
 
 	const strength = '23456789TJQKA'
-	const orderedHands = orderedByStrength(strength)
-
-	orderedHands.forEach((hand, index) => {
-		totalWinnings += hand.bid * (index + 1)
-	})
-
-	console.log('First level solution:\t', totalWinnings)
+	calcStrengthAndPrint(strength, 'First')
 }
 
+const secondLevel = () => {
+	for (const hand of hands) {
+		checkTypeWithJoker(hand)
+	}
+
+	const strengthWithJoker = 'J23456789TJQKA'
+	calcStrengthAndPrint(strengthWithJoker, 'Second')
+}
+
+const cards = new Map()
+
 firstLevel()
+totalWinnings = 0
+secondLevel()
