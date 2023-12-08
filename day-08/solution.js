fca805bfa688180c79803bf0a291e80de8778ce2
tmanso9 <tmanso92@gmail.com>
2023-12-08 13:47:53 +0000

    All done, only works for input.txt at the moment

--- day-08/solution.js
+++ day-08/solution.js
@@ -4,7 +4,7 @@ const data = readData('input.txt')
 
 const maps = new Map()
 const instructions = data[0].replaceAll('L', 0).replaceAll('R', 1)
-const queue = ['AAA']
+const queue = []
 let steps = 0
 
 const createMaps = () => {
@@ -30,12 +30,81 @@ const followMaps = () => {
 			queue.push(nextStep)
 		}
 	}
+	return steps
+}
+
+const setStartingPoint = () => {
+	Array.from(maps.keys()).forEach((name) => {
+		if (name.charAt(name.length - 1) === 'A') {
+			queue.push(name)
+		}
+	})
+
+	return queue.length
+}
+
+const individualRun = (tempQueue) => {
+	let moves = 0
+
+	while (tempQueue.length > 0) {
+		const here = tempQueue.shift()
+		const currentMap = maps.get(here)
+		const nextStep = currentMap[instructions[moves % instructions.length]]
+		moves++
+
+		if (nextStep.charAt(nextStep.length - 1) !== 'Z') {
+			tempQueue.push(nextStep)
+		}
+	}
+
+	return moves
+}
+
+const LCM = (arr, n) => {
+	const GCD = (a, b) => {
+		let x = Math.abs(a)
+		let y = Math.abs(b)
+		while (y) {
+			const t = y
+			y = x % y
+			x = t
+		}
+		return x
+	}
+
+	let ans = arr[0]
+
+	for (let i = 0; i < n; i++) {
+		ans = (arr[i] * ans) / GCD(arr[i], ans)
+	}
+
+	return ans
+}
+
+const followGhostMap = () => {
+	const solutions = []
+
+	for (let i = 0; i < queue.length; i++) {
+		const elem = queue[i]
+		solutions.push(individualRun([elem]))
+	}
+
+	steps = LCM(solutions, solutions.length)
 }
 
 const firstLevel = () => {
-	createMaps()
+	queue.push('AAA')
 	followMaps()
 	console.log('First level solution:\t', steps)
 }
 
+const secondLevel = () => {
+	steps = 0
+	setStartingPoint()
+	followGhostMap()
+	console.log('Second level solution:\t', steps)
+}
+
+createMaps()
 firstLevel()
+secondLevel()
