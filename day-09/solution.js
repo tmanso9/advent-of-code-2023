25fd4d3ea699ea7fb5ccee2ebcb086dd8569a9b8
tmanso9 <tmanso92@gmail.com>
2023-12-09 11:18:33 +0000

    All done

--- day-09/solution.js
+++ day-09/solution.js
@@ -1,15 +1,6 @@
 const { readData } = require('../utils/helper')
 
-const data = readData('sample.txt')
-
-const itemsHistory = []
-let sum = 0
-
-for (line of data) {
-	itemsHistory.push([line.split(' ').map(Number)])
-}
-
-for (item of itemsHistory) {
+const createHistory = (item) => {
 	while (!item[item.length - 1].every((val) => val === 0)) {
 		item.push(
 			item[item.length - 1]
@@ -21,45 +12,62 @@ for (item of itemsHistory) {
 				.filter((val) => val !== undefined)
 		)
 	}
+}
 
+const addNextValue = (item) => {
 	item.reverse()
 	item.forEach((value, i) => {
 		if (i === 0) value.push(0)
 		else value.push(value[value.length - 1] + item[i - 1][value.length - 1])
 	})
-
 	item.reverse()
-	const numOfElems = item[0].length
-	sum += item[0][numOfElems - 1]
 }
 
-const firstLevel = () => {
+const addPreviousValue = (item) => {
+	item.forEach((elem, index) => {
+		elem.reverse()
+		if (index === 0) elem.push(0)
+		else elem.push(elem[elem.length - 1] - item[index - 1][elem.length - 1])
+	})
+	item.forEach((elem) => {
+		elem.reverse()
+	})
+}
+
+const firstLevel = (itemsHistory) => {
+	let sum = 0
+	for (item of itemsHistory) {
+		addNextValue(item)
+		const numOfElems = item[0].length
+		sum += item[0][numOfElems - 1]
+	}
+
 	console.log('First level solution:\t', sum)
 }
 
-const secondLevel = () => {
-	sum = 0
+const secondLevel = (itemsHistory) => {
+	let sum = 0
 
 	for (item of itemsHistory) {
 		item.reverse()
-		item.forEach((elem, index) => {
-			elem.reverse()
-			if (index === 0) elem.push(0)
-			else
-				elem.push(
-					elem[elem.length - 1] - item[index - 1][elem.length - 1]
-				)
-		})
-		item.forEach((elem) => {
-			elem.reverse()
-		})
+		addPreviousValue(item)
 		item.reverse()
-		const numOfElems = item[0].length
 		sum += item[0][0]
 	}
 
 	console.log('Second level solution:\t', sum)
 }
 
-firstLevel()
-secondLevel()
+const main = () => {
+	const data = readData('input.txt')
+	const itemsHistory = []
+	data.forEach((line) => {
+		itemsHistory.push([line.split(' ').map(Number)])
+	})
+
+	itemsHistory.forEach(createHistory)
+	firstLevel(itemsHistory)
+	secondLevel(itemsHistory)
+}
+
+main()
