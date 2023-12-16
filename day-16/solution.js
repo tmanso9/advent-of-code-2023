39ff30384a59c99531129ec9962c9a508d3facca
tmanso9 <tmanso92@gmail.com>
2023-12-16 16:49:11 +0000

    Level 2 done

--- day-16/solution.js
+++ day-16/solution.js
@@ -1,7 +1,6 @@
 const { readData, printMap } = require('../utils/helper')
 
 const data = readData('input.txt')
-const seen = new Map()
 const queue = []
 
 const DIR = {
@@ -12,124 +11,132 @@ const DIR = {
 }
 let res = 0
 
-const firstLevel = () => {
-	const first = { p: { y: 0, x: 0 }, dir: DIR.right }
+const pushToQueue = (y, x, dir) => {
+	queue.push({ p: { y, x }, dir })
+}
+
+const splitVertically = (y, x) => {
+	goUp(y, x)
+	goDown(y, x)
+}
+
+const splitHorizontally = (y, x) => {
+	goLeft(y, x)
+	goRight(y, x)
+}
+
+const goLeft = (y, x) => {
+	pushToQueue(y + DIR.left.y, x + DIR.left.x, DIR.left)
+}
+
+const goRight = (y, x) => {
+	pushToQueue(y + DIR.right.y, x + DIR.right.x, DIR.right)
+}
+
+const goUp = (y, x) => {
+	pushToQueue(y + DIR.up.y, x + DIR.up.x, DIR.up)
+}
+
+const goDown = (y, x) => {
+	pushToQueue(y + DIR.down.y, x + DIR.down.x, DIR.down)
+}
+
+const keepGoing = (current) => {
+	pushToQueue(
+		current.p.y + current.dir.y,
+		current.p.x + current.dir.x,
+		current.dir
+	)
+}
+
+const runThroughCave = (first) => {
+	const seen = new Map()
 	queue.push(first)
 	while (queue.length) {
 		const current = queue.shift()
+
 		const { x, y } = current.p
 		const toCheck = `${y}-${x}`
-		const inSeen = seen.has(toCheck)
 		let beenThere = false
-		if (inSeen) {
+		if (seen.has(toCheck)) {
 			beenThere = seen.get(toCheck).includes(current.dir)
 		}
-		if (x < 0 || y < 0 || y >= data.length || x >= data[y].length || (inSeen && beenThere)) continue
-		let value = []
-		if (seen.has(toCheck)) {
-			value = seen.get(toCheck)
-			value.push(current.dir)
-		} else {
-			value.push(current.dir)
-		}
+
+		if (
+			x < 0 ||
+			y < 0 ||
+			y >= data.length ||
+			x >= data[y].length ||
+			beenThere
+		)
+			continue
+
+		let value = seen.has(toCheck) ? seen.get(toCheck) : []
+		value.push(current.dir)
 		seen.set(toCheck, value)
+
 		switch (data[y][x]) {
 			case '|':
-				if (current.dir === DIR.left || current.dir === DIR.right) {
-					queue.push({
-						p: { y: y + DIR.up.y, x: x + DIR.up.x },
-						dir: DIR.up
-					})
-					queue.push({
-						p: { y: y + DIR.down.y, x: x + DIR.down.x },
-						dir: DIR.down
-					})
-				} else {
-					queue.push({
-						p: { y: y + current.dir.y, x: x + current.dir.x },
-						dir: current.dir
-					})
-				}
+				current.dir === DIR.left || current.dir === DIR.right
+					? splitVertically(y, x)
+					: keepGoing(current)
 				break
 			case '-':
-				if (current.dir === DIR.up || current.dir === DIR.down) {
-					queue.push({
-						p: { y: y + DIR.left.y, x: x + DIR.left.x },
-						dir: DIR.left
-					})
-					queue.push({
-						p: { y: y + DIR.right.y, x: x + DIR.right.x },
-						dir: DIR.right
-					})
-				} else {
-					queue.push({
-						p: { y: y + current.dir.y, x: x + current.dir.x },
-						dir: current.dir
-					})
-				}
+				current.dir === DIR.up || current.dir === DIR.down
+					? splitHorizontally(y, x)
+					: keepGoing(current)
 				break
 			case '/':
-				if (current.dir === DIR.right) {
-					queue.push({
-						p: { y: y + DIR.up.y, x: x + DIR.up.x },
-						dir: DIR.up
-					})
-				} else if (current.dir === DIR.left) {
-					queue.push({
-						p: { y: y + DIR.down.y, x: x + DIR.down.x },
-						dir: DIR.down
-					})
-				} else if (current.dir === DIR.up) {
-					queue.push({
-						p: { y: y + DIR.right.y, x: x + DIR.right.x },
-						dir: DIR.right
-					})
-				} else {
-					queue.push({
-						p: { y: y + DIR.left.y, x: x + DIR.left.x },
-						dir: DIR.left
-					})
-				}
+				current.dir === DIR.right
+					? goUp(y, x)
+					: current.dir === DIR.left
+					? goDown(y, x)
+					: current.dir === DIR.up
+					? goRight(y, x)
+					: goLeft(y, x)
 				break
 			case '\\':
-				if (current.dir === DIR.right) {
-					queue.push({
-						p: { y: y + DIR.down.y, x: x + DIR.down.x },
-						dir: DIR.down
-					})
-				} else if (current.dir === DIR.left) {
-					queue.push({
-						p: { y: y + DIR.up.y, x: x + DIR.up.x },
-						dir: DIR.up
-					})
-				} else if (current.dir === DIR.up) {
-					queue.push({
-						p: { y: y + DIR.left.y, x: x + DIR.left.x },
-						dir: DIR.left
-					})
-				} else {
-					queue.push({
-						p: { y: y + DIR.right.y, x: x + DIR.right.x },
-						dir: DIR.right
-					})
-				}
+				current.dir === DIR.right
+					? goDown(y, x)
+					: current.dir === DIR.left
+					? goUp(y, x)
+					: current.dir === DIR.up
+					? goLeft(y, x)
+					: goRight(y, x)
 				break
 			default:
-				queue.push({
-					p: { y: y + current.dir.y, x: x + current.dir.x },
-					dir: current.dir
-				})
+				keepGoing(current)
 		}
 	}
-	// console.log(seen)
-	res = seen.size
+	return seen.size
+}
 
+const firstLevel = () => {
+	const first = { p: { y: 0, x: 0 }, dir: DIR.right }
+
+	res = runThroughCave(first)
 	console.log('First level solution:\t', res)
 }
 
+const exploreAndSetMax = (y, x, dir) => {
+	const first = { p: { y, x }, dir }
+	const energized = runThroughCave(first)
+	res = Math.max(res, energized)
+}
+
 const secondLevel = () => {
+	res = 0
+
+	for (let x = 0; x < data[0].length; x++) {
+		exploreAndSetMax(0, x, DIR.down)
+		exploreAndSetMax(data.length - 1, x, DIR.up)
+	}
+	for (let y = 0; y < data.length; y++) {
+		exploreAndSetMax(y, 0, DIR.right)
+		exploreAndSetMax(y, data[y].length - 1, DIR.left)
+	}
 	console.log('Second level solution:\t', res)
 }
 
 firstLevel()
-// secondLevel()
+secondLevel()
