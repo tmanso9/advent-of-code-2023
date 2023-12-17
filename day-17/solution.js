79fa9b4dad35232a15364ddb36699a13d84f0556
tmanso9 <tmanso92@gmail.com>
2023-12-17 16:01:04 +0000

    Level 1 done

---
+++ day-17/solution.js
@@ -0,0 +1,148 @@
+const { readData, printMap, DIR } = require('../utils/helper')
+const { PriorityQueue } = require('../utils/PriorityQueue')
+const data = readData('input.txt')
+
+class heatPriorityQueue extends PriorityQueue {
+	parent(index) {
+		return this.heap[this.getParentIndex(index)].heatLoss
+	}
+
+	leftChild(index) {
+		return this.heap[this.getLeftChildIndex(index)].heatLoss
+	}
+
+	rightChild(index) {
+		return this.heap[this.getRightChildIndex(index)].heatLoss
+	}
+
+	heapifyUp() {
+		let index = this.heap.length - 1
+		while (this.hasParent(index) && this.parent(index) > this.heap[index].heatLoss) {
+			this.swap(this.getParentIndex(index), index)
+			index = this.getParentIndex(index)
+		}
+	}
+
+	heapifyDown() {
+		let index = 0
+		while (this.hasLeftChild(index)) {
+			let smallerChildIndex = this.getLeftChildIndex(index)
+			if (
+				this.hasRightChild(index) &&
+				this.rightChild(index) < this.leftChild(index)
+			) {
+				smallerChildIndex = this.getRightChildIndex(index)
+			}
+			if (
+				this.heap[index].heatLoss <
+				this.heap[smallerChildIndex].heatLoss
+			) {
+				break
+			} else {
+				this.swap(index, smallerChildIndex)
+			}
+			index = smallerChildIndex
+		}
+	}
+
+	print() {
+		let str = ''
+		for (let i = 0; i < this.heap.length; i++) {
+			str += this.heap[i].heatLoss + ' '
+		}
+		console.log(str)
+	}
+}
+
+let res = 0
+
+const dijkstra = (input) => {
+	const seen = new Set()
+	const pq = new heatPriorityQueue()
+	pq.add({ heatLoss: 0, row: 0, col: 0, dirRow: 0, dirCol: 0, steps: 0 })
+
+	while (pq.heap.length > 0) {
+		const { heatLoss, row, col, dirRow, dirCol, steps } = pq.remove()
+		const storePosition = `[${row}][${col}], (${dirRow})(${dirCol}), ${steps}`
+
+		if (row === input.length - 1 && col === input[row].length - 1) {
+			return heatLoss
+		}
+
+		if (seen.has(storePosition)) continue
+		seen.add(storePosition)
+
+		if (steps < 3 && !(dirRow === 0 && dirCol === 0)) {
+			newRow = row + dirRow
+			newCol = col + dirCol
+			if (
+				newRow >= 0 &&
+				newRow < input.length &&
+				newCol >= 0 &&
+				newCol < input[newRow].length
+			)
+				pq.add({
+					heatLoss: heatLoss + input[newRow][newCol],
+					row: newRow,
+					col: newCol,
+					dirRow,
+					dirCol,
+					steps: steps + 1
+				})
+		}
+
+		const possibleDirections = [
+			[0, 1],
+			[0, -1],
+			[1, 0],
+			[-1, 0]
+		]
+
+		for (const [newDirRow, newDirCol] of possibleDirections) {
+			if (
+				!(newDirRow === dirRow && newDirCol === dirCol) &&
+				!(newDirRow === -dirRow && newDirCol === -dirCol)
+			) {
+				newRow = row + newDirRow
+				newCol = col + newDirCol
+				if (
+					newRow >= 0 &&
+					newRow < input.length &&
+					newCol >= 0 &&
+					newCol < input[newRow].length
+				)
+					pq.add({
+						heatLoss: heatLoss + input[newRow][newCol],
+						row: newRow,
+						col: newCol,
+						dirRow: newDirRow,
+						dirCol: newDirCol,
+						steps: 1
+					})
+			}
+		}
+	}
+	console.log(seen)
+	return 0
+}
+
+const firstLevel = () => {
+	const input = []
+	data.forEach((line, y) => {
+		if (line.length) {
+			input[y] = line.split('').map(Number)
+		}
+	})
+
+	// console.log(input)
+	res = dijkstra(input)
+
+	console.log('First level solution:\t', res)
+}
+
+const secondLevel = () => {
+	console.log('Second level solution:\t', res)
+}
+
+firstLevel()
+// secondLevel()
